a5ed879b588b58eddf74528049807a6b58601e5d
Oscar Gonzalez <oscarg610@gmail.com>
2018-08-15 19:39:56 -0400

    Add pagging support for latest jobs controller

--- src/controller/JobController.ts
+++ src/controller/JobController.ts
@@ -3,12 +3,46 @@ import { Job } from '../entity/Job';
 import BaseController from './BaseController';
 import {  JobForm } from '../form/JobForm';
 import BaseFormController from './BaseFormController';
+import { FindManyOptions } from 'typeorm';
 
 export class JobController extends BaseFormController<Job, JobForm> {
   public repo = this.db.getRepository(Job);
   public Form = JobForm;
 
+  public pageSize = 10;
+
   public async allLatest(req: Request, res: Response, next: NextFunction) {
-    return this.repo.find();
+    const findOpts: FindManyOptions<Job> = {
+      skip: 0,
+      take: this.pageSize,
+      where: {
+        active: true,
+      },
+      order: {
+        created: -1,
+        title: 1,
+      },
+    };
+
+    return this.repo.count({ active: true })
+    .then((count) => {
+      // @todo cache this
+      const totalPages = Math.ceil(count / this.pageSize);
+      let reqPage = Math.ceil(req.query.page) || 1;
+
+      if (reqPage > totalPages) {
+        reqPage = totalPages;
+      }
+
+      if (reqPage < 1) {
+        reqPage = 1;
+      }
+
+      findOpts.skip = this.pageSize * (reqPage - 1);
+
+      return this.repo
+        .find(findOpts)
+        .then((posts) => ({posts, totalPages, reqPage}));
+    });
   }
 }
